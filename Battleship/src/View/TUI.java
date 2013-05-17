dcce60ccfc388e8dd5a2a9a1c423346180225131
satonon <satonon@3DLABOR13.in.fhkn.de>
2013-05-17 17:31:38 +0200

    TUI

--- Battleship/src/View/TUI.java
+++ Battleship/src/View/TUI.java
@@ -12,15 +12,30 @@ public class TUI {
 	
 	public void showField(){
 		for(int i =0; i< fieldsize; i++){
-			System.out.printf("&d |", i);
+			if(i == 0){
+				System.out.print(" ");
+				for(int k = 65; k < fieldsize+65; k++){
+					System.out.printf(" | %s", (char) k);
+				}
+				System.out.println();
+			}
+			System.out.printf("&d | ", i);
 			for(int j =0; j< fieldsize; j++){
 				if(c.player.getPlayboard().getBoard()[i][j].getStat() == state.empty){
-					System.out.print("_|");
+					System.out.print("_ | ");
+				}
+				else if(c.player.getPlayboard().getBoard()[i][j].getStat() == state.emptyhit){
+					System.out.print("O | ");
+				}
+				else if(c.player.getPlayboard().getBoard()[i][j].getStat() == state.hit){
+					System.out.print("X | ");
+				}
+				else if(c.player.getPlayboard().getBoard()[i][j].getStat() == state.ship){
+					System.out.print("S | ");
 				}
 				
 			}
 			System.out.println("");
-			
 		}
 	}
 	
