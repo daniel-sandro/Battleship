ef9812700cd36e041042a74b3199d97492ec3435
Glur4k <tonon.san@gmail.com>
2013-06-08 12:54:06 +0200

    added some comments

--- Battleship/src/model/Destructor.java
+++ Battleship/src/model/Destructor.java
@@ -1,15 +1,31 @@
 package model;
 
+/**
+ * @author Sandro, Julian
+ * Destructor Class. Subclass of Ships.
+ */
 public class Destructor extends Ships {
 	
 	private static final int SIZE = 3;
 	
+	/**
+	 * Constructor for the bot's Destructor.
+	 * @param vertikal
+	 * @param horizontal
+	 */
 	public Destructor(boolean vertikal, boolean horizontal){
 		this.setSize(SIZE);
 		this.setVertical(vertikal);
 		this.setHorizontal(horizontal);
 	}
 	
+	/**
+	 * Constructor for the Human's Destructor.
+	 * @param row
+	 * @param col
+	 * @param vertikal
+	 * @param horizontal
+	 */
 	public Destructor(int row, int col, boolean vertikal, boolean horizontal) {
 		this.setSize(SIZE);
 		this.setVertical(vertikal);
--- Battleship/src/model/Flattop.java
+++ Battleship/src/model/Flattop.java
@@ -1,15 +1,31 @@
 package model;
 
+/**
+ * @author Sandro, Julian
+ * Flattop Class. Subclass of Ships.
+ */
 public class Flattop extends Ships {
 	
 	private static final int SIZE = 5;
 	
+	/**
+	 * Constructor for the bot's Flattop.
+	 * @param vertikal
+	 * @param horizontal
+	 */
 	public Flattop(boolean vertikal, boolean horizontal){
 		this.setSize(SIZE);
 		this.setVertical(vertikal);
 		this.setHorizontal(horizontal);
 	}
 	
+	/**
+	 * Constructor for the human's Flattop.
+	 * @param row
+	 * @param col
+	 * @param vertikal
+	 * @param horizontal
+	 */
 	public Flattop(int row, int col, boolean vertikal, boolean horizontal) {
 		this.setSize(SIZE);
 		setPosition(row, col);
